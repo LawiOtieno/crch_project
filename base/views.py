771d69476393cac08ecbd966ca6b88b2f67d663e
LAWRENCE <laurielawi12@gmail.com>
2021-12-20 09:03:18 +0300

    update room view

--- base/views.py
+++ base/views.py
@@ -1,25 +1,18 @@
 from django.shortcuts import render
+from .models import Room
 
 # Create your views here.
 
-rooms = [
-    {'id':1, 'name':'General Ruwe Room'},
-    {'id':2, 'name':'Ruwe Diocese Room'},
-    {'id':3, 'name':'Kisii-Nairobi Diocese Room'},
-    {'id':4, 'name':'Mombasa Diocese Room'},
-]
 
 def home(request):
+    rooms = Room.objects.all()
     context = {'rooms':rooms}
     return render(request, 'base/home.html', context)
 
 
 def room(request, pk):
-    room = None
+    room = Room.objects.get(id=pk)
 
-    for i in rooms:
-        if i['id'] == int(pk):
-            room = i
-
+    
     context = {'room':room}
     return render(request, 'base/room.html', context)
