869234e869a0cc607f12a970b107f497474d0109
LAWRENCE <laurielawi12@gmail.com>
2021-12-20 09:23:56 +0300

    create topic model

--- base/models.py
+++ base/models.py
@@ -1,12 +1,18 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-from base.views import room
 
 # Create your models here.
 
+class Topic(models.Model):
+    name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
+
 class Room(models.Model):
-    # host =
-    # topic = 
+    host = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+    topic = models.ForeignKey(Topic, on_delete=models.SET_NULL, null=True)
     name = models.CharField(max_length=250)
     description = models.TextField(null=True, blank=True)
     # participants =
@@ -17,7 +23,7 @@ class Room(models.Model):
         return self.name
 
 class Message(models.Model):
-    # user =
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
     room = models.ForeignKey(Room, on_delete=models.CASCADE)
     body = models.TextField()
     created = models.DateTimeField(auto_now_add=True)
