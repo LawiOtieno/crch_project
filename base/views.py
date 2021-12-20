011bf9282d0e28c80fef6b0ffe2337c89fa89fcf
LAWRENCE <laurielawi12@gmail.com>
2021-12-20 14:56:51 +0300

    create create-room template

--- base/views.py
+++ base/views.py
@@ -16,3 +16,9 @@ def room(request, pk):
     
     context = {'room':room}
     return render(request, 'base/room.html', context)
+
+
+def createRoom(request):
+
+    context = {}
+    return render(request, 'base/room_form.html', context)
