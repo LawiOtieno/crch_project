f9b4506fff989a2b6936f9b4bd04febc17e6ddfe
LAWRENCE <laurielawi12@gmail.com>
2021-12-20 09:11:18 +0300

    create message model

--- base/models.py
+++ base/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 
+from base.views import room
+
 # Create your models here.
 
 class Room(models.Model):
@@ -13,3 +15,14 @@ class Room(models.Model):
 
     def __str__(self):
         return self.name
+
+class Message(models.Model):
+    # user =
+    room = models.ForeignKey(Room, on_delete=models.CASCADE)
+    body = models.TextField()
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.body[0:50]
+
