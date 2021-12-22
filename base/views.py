b152496ae9cec413529276cbd7d3595f22a07280
LAWRENCE <laurielawi12@gmail.com>
2021-12-22 18:48:26 +0300

    create login

--- base/views.py
+++ base/views.py
@@ -4,10 +4,34 @@ from django.db.models import Q
 from .models import Room, Topic
 from .forms import RoomForm
 from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
+
 
 # Create your views here.
 
+
+def loginPage(request):
+
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        try:
+            user = User.objects.get(username=username)
+        except:
+            messages.error(request, 'No user found')
+        
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None:
+            login(request, user)
+            return redirect('home')
+        else:
+            messages.error(request, 'Username or Password does not exist.')
+
+    context ={}
+    return render(request, 'base/login_register.html', context)
 
 
 def home(request):
