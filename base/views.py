42546a99e84ebe1c66e159af42ccd534b0235369
LAWRENCE <laurielawi12@gmail.com>
2022-01-07 16:13:04 +0300

    room participants

--- base/views.py
+++ base/views.py
@@ -87,6 +87,7 @@ def home(request):
 def room(request, pk):
     room = Room.objects.get(id=pk)
     room_messages = room.message_set.all().order_by('-created')
+    participants = room.participants.all()
 
     if request.method == 'POST':
         message = Message.objects.create(
@@ -94,9 +95,10 @@ def room(request, pk):
             room = room,
             body = request.POST.get('body')
         )
+        room.participants.add(request.user) # Adding participant to a room automatically
         return redirect('room', pk=room.id)
     
-    context = {'room':room, 'room_messages':room_messages}
+    context = {'room':room, 'room_messages':room_messages, 'participants':participants}
     return render(request, 'base/room.html', context)
 
 
