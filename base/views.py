9dcc969a07e9c2ab2ce1abbef892887637f32884
LAWRENCE <laurielawi12@gmail.com>
2022-01-07 15:37:13 +0300

    room conversation

--- base/views.py
+++ base/views.py
@@ -2,7 +2,7 @@ from django import forms
 from django.http import HttpResponse
 from django.shortcuts import render,redirect
 from django.db.models import Q
-from .models import Room, Topic
+from .models import Message, Room, Topic
 from .forms import RoomForm
 from django.contrib.auth.models import User
 from django.contrib.auth import authenticate, login, logout
@@ -86,9 +86,17 @@ def home(request):
 
 def room(request, pk):
     room = Room.objects.get(id=pk)
+    room_messages = room.message_set.all().order_by('-created')
 
+    if request.method == 'POST':
+        message = Message.objects.create(
+            user = request.user,
+            room = room,
+            body = request.POST.get('body')
+        )
+        return redirect('room', pk=room.id)
     
-    context = {'room':room}
+    context = {'room':room, 'room_messages':room_messages}
     return render(request, 'base/room.html', context)
 
 
